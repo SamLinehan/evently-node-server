a834cabf2cf1f3ad647e9466924fb0dbe93b04ce
Samuel <samuel.linehan15@gmail.com>
2016-03-23 16:55:00 -0600

    safe commit

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 var Express = require('express')
 var app = Express()
 var server = require('http').Server(app)
-var io = require('socket.io')(server, {log:false, origins:'*:*'})
-// var io = require('socket.io').lister(server)
+var io = require('socket.io')(server)
 cors = require('cors')
 
 app.use(cors())
 
 
 app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-        next();
-    });
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
 
 
 server.listen(3000, function(){
