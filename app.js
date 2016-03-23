11fb937033c6c2c19bd874a3117ff31ca0196fe6
Samuel <samuel.linehan15@gmail.com>
2016-03-23 10:32:34 -0600

    i think i fixed cors

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ cors = require('cors')
 
 app.use(cors())
 
-io.set('origins', 'https://linehan-capstone.firebaseapp.com:*' );
+app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
+        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+        next();
+    });
 
 
 server.listen(3000, function(){
